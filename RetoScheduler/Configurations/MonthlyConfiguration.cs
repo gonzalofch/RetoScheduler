4510714a42b6003fdfe619f341b92cd11a326fd1
Gonzalo Flores Chuchon <gonzalo.flores@ext.semicrol.com>
2024-04-12 13:31:41 +0200

    ADDES CLASS MonthyConfiguration and ENUMS

---
+++ RetoScheduler/Configurations/MonthlyConfiguration.cs
@@ -0,0 +1,34 @@
+﻿using RetoScheduler.Configurations.Limits;
+using RetoScheduler.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RetoScheduler.Configurations
+{
+    public class MonthlyConfiguration
+    {
+
+        public MonthlyConfiguration(MonthlyConfigType type, int dayNumber, Ordinal ordinalNumber, KindOfDay kindOfDay, int frecuency)
+        {
+            Type = type;
+            DayNumber = (type == MonthlyConfigType.DayNumberOption) ? dayNumber : null;
+            OrdinalNumber = (type == MonthlyConfigType.WeekDayOption) ? ordinalNumber : null;
+            SelectedDay = (type == MonthlyConfigType.WeekDayOption) ? kindOfDay : null;
+            Frecuency = frecuency;
+        }
+
+        public MonthlyConfigType Type { get; set; }
+
+        public int? DayNumber { get; set; }
+
+        public Ordinal? OrdinalNumber { get; set; }
+
+        public KindOfDay? SelectedDay { get; set; }
+
+        public int Frecuency { get; set; }
+    }
+}
+
---
+++ RetoScheduler/Enums/MonthlyConfigType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RetoScheduler.Enums
+{
+    public enum MonthlyConfigType
+    {
+        DayNumberOption = 1,
+        WeekDayOption = 2,
+    }
+}
